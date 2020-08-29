03a6b736c56ea8373c2399d6a67659ab63ca25b1
anuj <anuj.thakur@appventurez.com>
2020-08-29 13:31:11 +0530

    fixed

--- cam/views.py
+++ cam/views.py
@@ -4,8 +4,4 @@ import os
 def Index(request):
     engine_no = request.POST
     print(engine_no)
-    try:
-        os.mkdir(os.path.join('/home/appventurez/Desktop/webcam/Data/', str(engine_no)))
-    except:
-        pass
     return render(request, 'index.html', {"engine_no": engine_no})
