98863328578a0f6c500fc4c31124a51cbffb614c
anuj <anuj.thakur@appventurez.com>
2020-08-29 11:40:55 +0530

    fixed webcam and create folder

--- cam/views.py
+++ cam/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
-
+import os
+ 
 def Index(request):
-    return render(request, 'index.html', {})
+    engine_no = request.POST
+    print(engine_no)
+    try:
+        os.mkdir(os.path.join('/home/appventurez/Desktop/webcam/Data/', str(engine_no)))
+    except:
+        pass
+    return render(request, 'index.html', {"engine_no": engine_no})
